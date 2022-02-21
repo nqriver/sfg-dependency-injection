5aa0d5a0d195277fc5f7a59405945b122cd27b01
nqriver <nqriver@gmail.com>
2022-02-21 11:13:21 +0100

    Using primary Beans and DI in Java Configuration

--- src/main/java/com/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,12 +1,39 @@
 package com.springframework.sfgdi.config;
 
-import com.springframework.sfgdi.services.ConstructorInjectedGreetingService;
-import com.springframework.sfgdi.services.SetterInjectedGreetingService;
+import com.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import com.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import com.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
+    @Profile({"PL"})
+    @Bean("i18nService")
+    I18nPolishGreetingService i18nPolishGreetingService() {
+        return new I18nPolishGreetingService();
+    }
+
+    @Profile({"EN"})
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
 
     @Bean
     ConstructorInjectedGreetingService constructorInjectedGreetingService() {
---
+++ src/main/java/com/springframework/sfgdi/repositories/EnglishGreetingRepository.java
@@ -0,0 +1,5 @@
+package com.springframework.sfgdi.repositories;
+
+public interface EnglishGreetingRepository {
+    String getGreeting();
+}
